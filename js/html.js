ddd4a9e55936c3abd9507925230be7c916ae84b0
Vector <yaroslav.drapak.i@gmail.com>
2024-12-30 19:23:33 +0200

    new Calculate file

--- js/html.js
+++ js/html.js
@@ -1,3 +1,16 @@
 function hasClass(element, className) {
-    return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+    return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1
+}
+function createElementWithClass(tag, classNameArr) {
+    var element = document.createElement(tag)
+    classNameArr.forEach(className => {
+        element.classList.add(className)
+    })
+    return element
+}
+function appendElements(parent, childrenArr) {
+    childrenArr.forEach(child => {
+        parent.append(child)
+    })
+    return parent
 }
